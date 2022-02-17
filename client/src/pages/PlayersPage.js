7f3b1efd0addd3b34e038672b8a9d831f2e425c2
Raunaq Singh <raunaqsingh2020@gmail.com>
2022-02-17 03:42:14 -0500

    update players page

--- client/src/pages/PlayersPage.js
+++ client/src/pages/PlayersPage.js
@@ -41,8 +41,25 @@ const playerColumns = [
         key: 'Rating',
         sorter: (a, b) => a.Rating - b.Rating
 
-    }
+    },
     // TASK 19: copy over your answers for tasks 7 - 9 to add columns for potential, club, and value
+    {
+        title: 'Potential',
+        dataIndex: 'Potential',
+        key: 'Potential',
+        sorter: (a, b) => a.Potential - b.Potential
+    },
+    {
+        title: 'Club',
+        dataIndex: 'Club',
+        key: 'Club',
+        sorter: (a, b) => a.Club.localeCompare(b.Club)
+    },
+    {
+        title: 'Value',
+        dataIndex: 'Value',
+        key: 'Value'
+    }
 ];
 
 
@@ -79,10 +96,12 @@ class PlayersPage extends React.Component {
 
     handleClubQueryChange(event) {
         // TASK 20: update state variables appropriately. See handleNameQueryChange(event) for reference
+        this.setState({ clubQuery: event.target.value })
     }
 
     handleNationalityQueryChange(event) {
         // TASK 21: update state variables appropriately. See handleNameQueryChange(event) for reference
+        this.setState({ nationalityQuery: event.target.value })
     }
 
     handleRatingChange(value) {
@@ -92,6 +111,8 @@ class PlayersPage extends React.Component {
 
     handlePotentialChange(value) {
         // TASK 22: parse value and update state variables appropriately. See handleRatingChange(value) for reference
+        this.setState({ potLowQuery: value[0] })
+        this.setState({ potHighQuery: value[1] })
     }
 
 
@@ -99,7 +120,9 @@ class PlayersPage extends React.Component {
     updateSearchResults() {
 
         //TASK 23: call getPlayerSearch and update playerResults in state. See componentDidMount() for a hint
-
+        getPlayerSearch(this.state.nameQuery, this.state.nationalityQuery, this.state.clubQuery, this.state.ratingHighQuery, this.state.ratingLowQuery, this.state.potHighQuery, this.state.potLowQuery, null, null).then(res => {
+            this.setState({ playersResults: res.results })
+        })
     }
 
     componentDidMount() {
@@ -109,7 +132,9 @@ class PlayersPage extends React.Component {
 
         // TASK 25: call getPlayer with the appropriate parameter and set update the correct state variable. 
         // See the usage of getMatch in the componentDidMount method of MatchesPage for a hint! 
-
+        getPlayer(this.state.selectedPlayerId).then(res => {
+            this.setState({ selectedPlayerDetails: res.results[0] })
+        })
     }
 
     render() {
@@ -129,7 +154,10 @@ class PlayersPage extends React.Component {
                             <FormInput placeholder="Nationality" value={this.state.nationalityQuery} onChange={this.handleNationalityQueryChange} />
                         </FormGroup></Col>
                         {/* TASK 26: Create a column for Club, using the elements and style we followed in the above two columns. Use the onChange method (handleClubQueryChange)  */}
-
+                        <Col flex={2}><FormGroup style={{ width: '20vw', margin: '0 auto' }}>
+                            <label>Club</label>
+                            <FormInput placeholder="Club" value={this.state.clubQuery} onChange={this.handleClubQueryChange} />
+                        </FormGroup></Col>
                     </Row>
                     <br></br>
                     <Row>
@@ -139,6 +167,10 @@ class PlayersPage extends React.Component {
 
                         </FormGroup></Col>
                         {/* TASK 27: Create a column with a label and slider in a FormGroup item for filtering by Potential. See the column above for reference and use the onChange method (handlePotentialChange)  */}
+                        <Col flex={2}><FormGroup style={{ width: '20vw', margin: '0 auto' }}>
+                            <label>Potential</label>
+                            <Slider range defaultValue={[50, 100]} onChange={this.handlePotentialChange} />
+                        </FormGroup></Col>
                         <Col flex={2}><FormGroup style={{ width: '10vw' }}>
                             <Button style={{ marginTop: '4vh' }} onClick={this.updateSearchResults}>Search</Button>
                         </FormGroup></Col>
@@ -149,7 +181,7 @@ class PlayersPage extends React.Component {
                 </Form>
                 <Divider />
                 {/* TASK 24: Copy in the players table from the Home page, but use the following style tag: style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }} - this should be one line of code! */}
-
+                <Table style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }} dataSource={this.state.playersResults} columns={playerColumns} pagination={{ pageSizeOptions:[5, 10], defaultPageSize: 5, showQuickJumper:true }}/>
                 <Divider />
 
                 {this.state.selectedPlayerDetails ? <div style={{ width: '70vw', margin: '0 auto', marginTop: '2vh' }}>
@@ -185,6 +217,12 @@ class PlayersPage extends React.Component {
                                 Age: {this.state.selectedPlayerDetails.Age}
                                 </Col>
                                 {/* TASK 28: add two more columns here for Height and Weight, with the appropriate labels as above */}
+                                <Col>
+                                Height: {this.state.selectedPlayerDetails.Height}
+                                </Col>
+                                <Col>
+                                Weight: {this.state.selectedPlayerDetails.Weight}
+                                </Col>
                                 <Col flex={2} style={{ textAlign: 'right' }}>
                                 {this.state.selectedPlayerDetails.Nationality}
                                     <img src={this.state.selectedPlayerDetails.Flag} referrerpolicy="no-referrer" alt={null} style={{height:'3vh', marginLeft: '1vw'}}/>
@@ -199,6 +237,12 @@ class PlayersPage extends React.Component {
                                 Release Clause: {this.state.selectedPlayerDetails.ReleaseClause}
                                 </Col>
                                 {/* TASK 29: Create 2 additional columns for the attributes 'Wage' and 'Contract Valid Until' (use spaces between the words when labelling!) */}
+                                <Col>
+                                Wage: {this.state.selectedPlayerDetails.Wage}
+                                </Col>
+                                <Col>
+                                Contract Valid Until: {this.state.selectedPlayerDetails.ContractValidUntil}
+                                </Col>
                             </Row>
                         </CardBody>
 
@@ -212,31 +256,47 @@ class PlayersPage extends React.Component {
                             <Rate disabled defaultValue={this.state.selectedPlayerDetails.Skill} />
                             <h6>Reputation</h6>
                             {/* TASK 30: create a star rating component for 'InternationalReputation'. Make sure you use the 'disabled' option as above to ensure it is read-only*/}
+                            <Rate disabled defaultValue={this.state.selectedPlayerDetails.InternationalReputation} />
                             <Divider/>
                             <h6>Best Rating</h6>
-                                <Progress style={{ width: '20vw'}} value={this.state.selectedPlayerDetails.BestOverallRating} >{this.state.selectedPlayerDetails.BestOverallRating}</Progress>
-                                {/* TASK 31: create the headings and progress bars for 'Potential' and 'Rating'. Use the same style as the one above for 'Best Rating'.*/}
-                                </Col >
-                                <Col  push={2} flex={2}>
+                            <Progress style={{ width: '20vw'}} value={this.state.selectedPlayerDetails.BestOverallRating} >{this.state.selectedPlayerDetails.BestOverallRating}</Progress>
+                            {/* TASK 31: create the headings and progress bars for 'Potential' and 'Rating'. Use the same style as the one above for 'Best Rating'.*/}
+                            <h6>Potential</h6>
+                            <Progress style={{ width: '20vw'}} value={this.state.selectedPlayerDetails.Potential} >{this.state.selectedPlayerDetails.Potential}</Progress>
+                            <h6>Rating</h6>
+                            <Progress style={{ width: '20vw'}} value={this.state.selectedPlayerDetails.Rating} >{this.state.selectedPlayerDetails.Rating}</Progress>
+                            </Col >
+                                <Col push={2} flex={2}>
                                 {/*TASK 32: In case the player is a GK, show a radar chart (replacing 'null' below) with the labels: Agility, Ball Control, Passing, Positioning, Stamina, Strength */}
-
-                                    {this.state.selectedPlayerDetails.BestPosition === 'GK'?null:<RadarChart
-                                data={[this.state.selectedPlayerDetails]}
-                                tickFormat={t => wideFormat(t)}
-                                startingAngle={0}
-                                domains={[
-                                    { name: 'Agility', domain: [0, 100], getValue: d => d.NAdjustedAgility },
-                                    { name: 'Ball Control', domain: [0, 100], getValue: d => d.NBallControl },
-                                    { name: 'Passing', domain: [0, 100], getValue: d => d.NPassing },
-                                    { name: 'Positioning', domain: [0, 100], getValue: d => d.NPositioning },
-                                    { name: 'Stamina', domain: [0, 100], getValue: d => d.NStamina },
-                                    { name: 'Strength', domain: [0, 100], getValue: d => d.NStrength }
-                                ]}
-                                width={450}
-                                height={400}
-                                
-                            />}
-                                
+                                    {this.state.selectedPlayerDetails.BestPosition === 'GK'? <RadarChart
+                                        data={[this.state.selectedPlayerDetails]}
+                                        tickFormat={t => wideFormat(t)}
+                                        startingAngle={0}
+                                        domains={[
+                                            { name: 'Penalties', domain: [0, 100], getValue: d => d.GKPenalties },
+                                            { name: 'Diving', domain: [0, 100], getValue: d => d.GKDiving },
+                                            { name: 'Handling', domain: [0, 100], getValue: d => d.GKHandling },
+                                            { name: 'Kicking', domain: [0, 100], getValue: d => d.GKKicking },
+                                            { name: 'Positioning', domain: [0, 100], getValue: d => d.GKPositioning },
+                                            { name: 'Reflexes', domain: [0, 100], getValue: d => d.GKReflexes }
+                                        ]}
+                                        width={450}
+                                        height={400}
+                                    /> : <RadarChart
+                                        data={[this.state.selectedPlayerDetails]}
+                                        tickFormat={t => wideFormat(t)}
+                                        startingAngle={0}
+                                        domains={[
+                                            { name: 'Agility', domain: [0, 100], getValue: d => d.NAdjustedAgility },
+                                            { name: 'Ball Control', domain: [0, 100], getValue: d => d.NBallControl },
+                                            { name: 'Passing', domain: [0, 100], getValue: d => d.NPassing },
+                                            { name: 'Positioning', domain: [0, 100], getValue: d => d.NPositioning },
+                                            { name: 'Stamina', domain: [0, 100], getValue: d => d.NStamina },
+                                            { name: 'Strength', domain: [0, 100], getValue: d => d.NStrength }
+                                        ]}
+                                        width={450}
+                                        height={400}
+                                    />}
                                 </Col>
                             </Row>
                         </CardBody>
